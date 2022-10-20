ecf29a556e0caccb580b3d973312293265b70e4e
ManeJS <116288258+ManeJS@users.noreply.github.com>
2022-10-20 08:40:37 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title("Mi aplicación")
 st.sidebar.header("Esta aplicación es solo un demo")
-st.sidebar.image("http://fcq.uach.mx/index.php/institucionales2-0/columna-2/escudos-institucionales/")
+st.sidebar.image("http://fcq.uach.mx/images/institucionales/Escudos/Dorado.png")
 boton = st.button("globos")
 if boton:
   st.balloon()
