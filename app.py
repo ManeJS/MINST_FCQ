59e2c189f34dd0d5a6fbb66a5d7caf21b34ac153
ManeJS <116288258+ManeJS@users.noreply.github.com>
2022-10-20 09:00:08 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 import streamlit as st
-st.title("Mi aplicación")
-st.sidebar.header("Esta aplicación es solo un demo")
-st.sidebar.image("http://fcq.uach.mx/images/institucionales/Escudos/Dorado.png")
-boton = st.button("globos")
-if boton:
-  st.balloons()
+from img_classification import teachable_machine_classification
+import keras
+from PIL import Image, ImageOps
+import numpy as np
+
+st.title("Aplicación de reconocimiento de celúlas sanguíneas")
+
+st.balloons()
+
+uploaded_file = st.file_uploader("Carga una imagen ...", type=["jpg","jpeg"])
+if uploaded_file is not None:
+  image = Image.open(uploaded_file)
+  st.sidebar.image(image)
+  label = teachable_machine_classification(image, 'keras_model.h5') 
+  if label == 0:
+    st.write("EOSINOFILO")
+  elif label == 1:
+    st.write("LINFOCITO")
+  else:
+    st.write("MONOCITO")
